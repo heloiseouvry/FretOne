96b54e87b30ebdb10c8a17735cb57d3e0863a092
Heloise Ouvry <heloiseouvry@gmail.com>
2022-02-16 15:05:00 +0100

    Naming correction (File>Upload)

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from app import db
 
-class File(db.Model):
+class Upload(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(200), index=True)
 
     def __repr__(self):
-        return f'<File {self.filename}>'
+        return f'<Upload {self.filename}>'
