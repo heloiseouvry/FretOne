f4f1a2fd951da82ee2e21e93569f03209b284398
Heloise Ouvry <heloiseouvry@gmail.com>
2022-02-17 19:10:36 +0100

    Install WhiteNoise

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from whitenoise import WhiteNoise
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_object(Config)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
