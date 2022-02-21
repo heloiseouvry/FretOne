1fcab813b66d1a26a2c205fbfb2553ab3a7df7bf
Heloise Ouvry <heloiseouvry@gmail.com>
2022-02-21 13:46:55 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,14 @@ from flask import Flask
 from whitenoise import WhiteNoise
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-try:
-    from config import Config
-except ModuleNotFoundError as err:
-    print(err)
+
 
 app = Flask(__name__, static_url_path='/static')
-print(app.config)
-if Config:
+try:
+    from config import Config
     app.config.from_object(Config)
+except ModuleNotFoundError as err:
+    print(err)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
