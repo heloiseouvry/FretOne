962a94113ebe801b3770db1838ef9438c8438ed7
Heloise Ouvry <heloiseouvry@gmail.com>
2022-02-21 12:23:04 +0100

    Process upload to/from S3 bucket

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,9 @@ from app import db
 from app.forms import SheetForm
 from app.models import Upload
 from tab2notes.src.f1 import translate
+import boto3
+from botocore.exceptions import ClientError
+import logging
 
 @app.route('/')
 @app.route('/index')
@@ -15,20 +18,47 @@ def index():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    print(app.root_path)
     form = SheetForm()
     if form.validate_on_submit():
         f = form.fileloader.data
         filename = secure_filename(f.filename)
         f.save(os.path.join('/tmp', filename))
-        f_db = Upload(filename=filename)
-        db.session.add(f_db)
-        db.session.commit()
-        translate(os.path.join('/tmp', filename), os.path.join('/tmp'))
-        # return redirect(url_for('upload_db'))
-        return render_template('upload.html', form=form, os=os)
 
-    return render_template('upload.html', form=form, os=os)
+        if app.config:
+            S3_BUCKET = app.config['S3_BUCKET_NAME']
+            S3_KEY = app.config['AWS_ACCESS_KEY_ID']
+            S3_SECRET = app.config['AWS_SECRET_ACCESS_KEY']
+        else:
+            S3_BUCKET = os.environ.get('S3_BUCKET_NAME')
+            S3_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+            S3_SECRET = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id=S3_KEY,
+            aws_secret_access_key=S3_SECRET
+        )
+
+        try:
+            s3.upload_file(os.path.join('/tmp', filename), S3_BUCKET, filename)
+        except ClientError as e:
+            logging.error(e)
+
+        if not s3:
+            f_db = Upload(filename=filename)
+            db.session.add(f_db)
+            db.session.commit()
+
+        translated_filename = translate(os.path.join('/tmp', filename), os.path.join('/tmp/'))
+
+        try:
+            s3.upload_file(os.path.join('/tmp', translated_filename), S3_BUCKET, translated_filename)
+        except ClientError as e:
+            logging.error(e)
+
+        return render_template('upload.html', form=form, os=os, S3_BUCKET=S3_BUCKET)
+
+    return render_template('upload.html', form=form, os=os, S3_BUCKET=None)
 
 @app.route('/upload_db')
 def upload_db():
